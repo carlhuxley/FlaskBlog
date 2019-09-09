e3706ab2767c35491e14bd158b3b05cb3f084454
Carl Huxley <carlhuxley@gmail.com>
2019-09-09 09:13:24 +0100

    Initial Commit

---
+++ flaskblog/routes.py
@@ -0,0 +1,52 @@
+from flask import render_template, url_for, flash, redirect
+from flaskblog import app, db, bcrypt
+from flaskblog.forms import RegistrationForm, LoginForm
+from flaskblog.models import User, Post
+
+posts = [
+	{
+		'author':'Carl Huxley',
+		'title':'Blog Post 1',
+		'content':'First Blog Content',
+		'date_posted':'5th September 2019'
+	},
+	{
+		'author':'Jane Doe',
+		'title':'Blog Post 2',
+		'content':'Second Blog Content',
+		'date_posted':'6th September 2019'
+	}
+]
+
+
+@app.route("/")
+@app.route("/home")
+def home():
+    return render_template('home.html', posts=posts)
+
+@app.route("/about")
+def about():
+    return render_template('about.html', title='About')
+
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+	form = RegistrationForm()
+	if form.validate_on_submit():
+		hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
+		user = User(username=form.username.data, email=form.email.data, password=hashed_password)
+		db.session.add(user)
+		db.session.commit()
+		flash(f'Your account has been created! You are now able to log in.', 'success')
+		return redirect(url_for('login'))
+	return render_template('register.html', title='Register', form=form)
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+	form = LoginForm()
+	if form.validate_on_submit():
+		if form.email.data == 'admin@blog.com' and form.password.data == 'password':
+			flash('You have been logged in!', 'success')
+			return redirect(url_for('home'))
+		else:
+			flash('Login Unsuccessful. Please check username and password.', 'danger')
+	return render_template('login.html', title='Login', form=form)
---
+++ run.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep  5 10:42:46 2019
+
+@author: JMN
+"""
+from flaskblog import app
+
+if __name__ == '__main__':
+    app.run(debug=True)
