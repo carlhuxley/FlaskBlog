a9953e65858a95442913c9f44c27b392f4179e93
Carl Huxley <carlhuxley@gmail.com>
2019-09-16 12:20:43 +0100

    Add Blueprints and Configuration

--- run.py
+++ run.py
@@ -4,7 +4,9 @@ Created on Thu Sep  5 10:42:46 2019
 
 @author: JMN
 """
-from flaskblog import app
+from flaskblog import create_app
+
+app = create_app()
 
 if __name__ == '__main__':
     app.run(debug=True)
